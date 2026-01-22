ec6dd736adb18baf28dcf09a6379eb2079c8b139
Birkan A.F. <59991587+ksk1kan@users.noreply.github.com>
2026-01-22 20:31:14 +0300

    v1

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = "rentacar-offline-v4-2-2";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll([
+      "./",
+      "./index.html",
+      "./manifest.webmanifest",
+      "./sw.js",
+      "./BAF_logo.png"
+    ])).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE) ? caches.delete(k) : null))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((res) => {
+      try{
+        if (event.request.method === "GET" && new URL(event.request.url).origin === location.origin) {
+          const copy = res.clone();
+          caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        }
+      }catch(_){}
+      return res;
+    }).catch(() => cached))
+  );
+});
